7b794a6e0f323cc20f1cadc31c6911bd8c4879d1
benjfrench <benfrerch@live.com.au>
2024-07-08 08:14:53 +0800

    Exercise-1

--- myapp/index.js
+++ myapp/index.js
@@ -1,16 +1,24 @@
 const express = require("express");
-const app = express();
-const port = 3000;
+const app1 = express();
+const port1 = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const app2 = express()
+const port2 = 3001
+
+app1.get("/", (req, res) => {
+  res.send("Hello World! App 1");
 });
 
-app.get("/test", (req, res) => {
-    res.send("This is a test");
+app2.get("/", (req, res) => {
+    res.send("App 2");
   });
 
-app.listen(port, () => {
+app1.listen(port1, () => {
   console.log(`Example app listening 
-at http://localhost:${port}`);
+at http://localhost:${port1}`);
 });
+
+app2.listen(port2, () => {
+    console.log(`Example app listening 
+  at http://localhost:${port2}`);
+  });
